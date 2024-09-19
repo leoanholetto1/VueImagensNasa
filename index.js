5dfebdc8644167655c03d386511a00a9150a0427
leoanholetto1 <leogomesanholetto@gmail.com>
2024-09-18 22:06:19 -0300

    Inicio do Projeto

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors("*"));
+
+
+app.listen(8081, () => {
+    console.log("Servidor na porta 8081");   
+});
